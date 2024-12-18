f3b906739310dec863fc4e6a3bb3e2fbf30b8452
wql2002 <wqlthu2020@gmail.com>
2024-12-18 08:25:16 +0000

    feat(server): handle client and memory moving requests

--- include/Server.hpp
+++ include/Server.hpp
@@ -4,11 +4,16 @@
 #include <string>
 
 #include "RingBuffer.hpp"
+#include "PageTable.hpp"
 #include "Common.hpp"
+#include "Utils.hpp"
 
 class Server {
 public:
-    Server(RingBuffer<ClientMessage>& client_buffer, RingBuffer<MemMoveReq>& move_page_buffer_, const std::vector<int>& client_memory_spaces);
+    Server(RingBuffer<ClientMessage>& client_buffer, RingBuffer<MemMoveReq>& move_page_buffer_, 
+        const std::vector<int>& client_memory_spaces);
+    void handleClientMessage(const ClientMessage& msg);
+    void handleMemoryMoveRequest(const MemMoveReq& req);
     void runManagerThread();
     void runPolicyThread();
     void start();
@@ -17,6 +22,7 @@ private:
     RingBuffer<ClientMessage>& client_buffer_;
     RingBuffer<MemMoveReq>& move_page_buffer_;
     std::vector<int> base_addresses_;
+    PageTable* page_table_;
 };
 
 #endif // SERVER_H
